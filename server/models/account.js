051e3a3a890b5b22c7bf65d24631897e0fbefd2e
Corentin Giraud <corentin.giraud@insa-lyon.fr>
2018-04-23 11:54:44 +0200

    initial commit

---
+++ server/models/account.js
@@ -0,0 +1,36 @@
+export const account = {
+    id: 'idAccount',
+    connexion: {
+        email: 'noel.flantier@insa-lyon.fr', // Optional
+        password: 'encryptedPassword' // Optional
+    },
+    userInformation: {
+        lastName: 'Flantier', // Server side only and optional
+        firstName: 'Noël', // Server side only and optional
+        dateOfBirth: 'DATE', // Server side only and optional
+        username: 'nFlantier',
+        accountType: 'ADMIN, EDITOR, GAMER'
+    },
+    dates: {
+        createdAt: '',
+        updatedAt: ''
+    },
+    game: {
+        badges: [
+            'idBadge1',
+            'idBadge2'
+        ],
+        quests: [
+            {
+                idQuest: 'idQuest',
+                state: 'ENUM[IN_PROGRESS, DONE]',
+                stats: {
+                    earnedXp: 10,
+                },
+                idFeedback: ''
+            }
+        ],
+        xp: 0,
+        elapsedTime: 123 //sec
+    }
+}
