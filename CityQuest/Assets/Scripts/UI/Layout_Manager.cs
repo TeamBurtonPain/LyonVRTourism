0c5c6b5f302cd9019ba539809eba3c112b13711d
HubertHamelin <32563790+HubertHamelin@users.noreply.github.com>
2018-04-24 15:14:50 +0200

    Layout v1
    Première version du Layout (un gameobject réutilisable) qui sera sur toutes les pages (sauf la page de login)

---
+++ CityQuest/Assets/Scripts/UI/Layout_Manager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Layout_Manager : MonoBehaviour {
+
+    public Button menu;
+    public Color backgroundColor;
+    public GameObject layout;
+    public GameObject menuDeroulant;
+
+    public void Awake()
+    {
+        menuDeroulant.SetActive(false);
+        ColorManager();
+    }
+
+    public void ColorManager()
+    {
+        backgroundColor = Color.gray;
+        layout.GetComponent<Image>().color = backgroundColor;
+        menuDeroulant.GetComponent<Image>().color = backgroundColor;
+    }
+
+    public void OpenMenu()
+    {
+        menuDeroulant.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        menuDeroulant.SetActive(false);
+    }
+
+    public void MenuDeroulantManager()
+    {
+        if (menuDeroulant.activeSelf)
+        {
+            CloseMenu();
+        }
+        else
+        {
+            OpenMenu();
+        }
+    }
+
+    public void SelectButtonMesQuetesManager()
+    {
+        SceneManager.LoadScene("Mes quêtes");
+    }
+    public void SelectButtonParametresManager()
+    {
+        SceneManager.LoadScene("Paramètres");
+    }
+}
