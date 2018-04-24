e7e9a6bb9359f879c92dfab811fdbbba93bb0ddf
pLOPeGG <douzont@gmail.com>
2018-04-24 10:17:02 +0200

    TD : Retrait des tuples, ajout d'une classe coordonates

--- CityQuest/Assets/Scripts/Model/Quest.cs
+++ CityQuest/Assets/Scripts/Model/Quest.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Assets.Scripts.Model
 {
+    class Coordonates
+    {
+        private Decimal x;
+        private Decimal y;
+    }
     class Quest
     {
         private long id;
-        private Tuple<Decimal, Decimal> geolocalisation;
+        private Coordonates geolocalisation;
         private string description;
         private StatsQuest statistics;
         private Boolean open;
