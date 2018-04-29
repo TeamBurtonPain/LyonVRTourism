b4112761e0b1979bb2fe519c703fee35f8537675
Morelsan <morel.victor.69@gmail.com>
2018-04-29 16:17:00 +0200

    [VM] Account Creation Scene

--- CityQuest/Assets/Scripts/UI/AccountCreationManager.cs
+++ CityQuest/Assets/Scripts/UI/AccountCreationManager.cs
@@ -1,40 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class AccountCreationManager : MonoBehaviour
 {
-    public void Btn_SelectMenuNewQuest()
-    {
-        Controller.Instance.SelectMenuNewQuest();
-    }
-    public void Btn_SelectMenuHistoric()
-    {
-        Controller.Instance.SelectMenuHistoric();
-    }
-    public void Btn_SelectMenuSettings()
-    {
-        Controller.Instance.SelectMenuSettings();
-    }
-    public void Btn_LogOut()
-    {
-        Controller.Instance.SelectMenuLogout();
-    }
-    public void Btn_AskLeave()
-    {
-        Controller.Instance.AskLeave();
-    }
+    public Button CreateAccountButton;
+    public InputField FirstNameInputField;
+    public InputField LastNameInputField;
+    public InputField MailInputField;
+    public InputField PasswordInputField;
+    public Dropdown YearsDropdown;
+    public Dropdown MonthsDropdown;
+    public Dropdown DaysDropdown;
 
-    public void Btn_LoginLocal()
-    {
-        Controller.Instance.LoginLocal();
-    }
 
-    public void Btn_LoginServer()
-    {
-        Controller.Instance.LoginServer();
-    }
 
-    public void Btn_Inscription()
-    {
-        Controller.Instance.Inscription();
-    }
 }
