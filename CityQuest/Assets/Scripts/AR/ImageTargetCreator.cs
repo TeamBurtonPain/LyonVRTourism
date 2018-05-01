3524a7269dc2c5576818b6acf64e903d3cdf446e
Thomas S <thomas.sassano@insa-lyon.fr>
2018-05-01 12:10:50 +0100

    [TS] Reconnaissance de contenu de la base de donnée opérationnel
    - Détection d'objet quelconque fixe ok (sous plusieurs plan en fonction des photos prises)
    - Ajout d'image target dynamique à la scene (reprend les éléments de la base de données entrée en paramètres du script)

---
+++ CityQuest/Assets/Scripts/AR/ImageTargetCreator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetCreator : MonoBehaviour
+{
+
+    public string dataSetName = "TestDB";
+
+	// Use this for initialization
+	void Start () {
+	    VuforiaARController.Instance.RegisterVuforiaStartedCallback(LoadDataSet);
+	}
+
+    void LoadDataSet()
+    {
+        ObjectTracker objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+
+        DataSet dataSet = objectTracker.CreateDataSet();
+
+        if (dataSet.Load(dataSetName))
+        {
+
+            objectTracker.Stop();  // stop tracker so that we can add new dataset
+
+            if (!objectTracker.ActivateDataSet(dataSet))
+            {
+                // Note: ImageTracker cannot have more than 100 total targets activated
+                Debug.Log("<color=yellow>Failed to Activate DataSet: " + dataSetName + "</color>");
+            }
+
+            if (!objectTracker.Start())
+            {
+                Debug.Log("<color=yellow>Tracker Failed to Start.</color>");
+            }
+            
+            int counter = 0;
+
+            IEnumerable<TrackableBehaviour> tbs = TrackerManager.Instance.GetStateManager().GetTrackableBehaviours();
+            foreach (TrackableBehaviour tb in tbs)
+            {
+                if (tb.name == "New Game Object")
+                {
+
+                    // change generic name to include trackable name
+                    tb.gameObject.name = ++counter + ":DynamicImageTarget-" + tb.TrackableName;
+
+                    // add additional script components for trackable
+                    tb.gameObject.AddComponent<Detection>();
+                    tb.gameObject.AddComponent<TurnOffBehaviour>();
+
+                }
+            }
+            
+        }
+        else
+        {
+            Debug.LogError("<color=yellow>Failed to load dataset: '" + dataSetName + "'</color>");
+        }
+    }
+}
