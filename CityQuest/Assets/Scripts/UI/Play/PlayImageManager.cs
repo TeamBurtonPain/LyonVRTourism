6f1f78c5ee975e04a0f1164cb153fe07a99992dc
cpourcel <clara.pourcel@insa-lyon.fr>
2018-05-03 17:15:18 +0200

    [CP] Image quete propre

--- CityQuest/Assets/Scripts/UI/Play/PlayImageManager.cs
+++ CityQuest/Assets/Scripts/UI/Play/PlayImageManager.cs
@@ -4,7 +4,7 @@ using System;
 
 class PlayImageManager : MonoBehaviour
 {
-    //public Image researchImage;
+    public Image researchImage;
 
     private void Start()
     {
@@ -12,21 +12,20 @@ class PlayImageManager : MonoBehaviour
         string picture = PlayQuestController.Instance.CurrentCheckpoint.Checkpoint.Picture;
         Char delimiter = ',';
         String[] substrings = picture.Split(delimiter);
-        Debug.Log(substrings[1]);
         byte[] img;
         try
         {
-            Debug.Log("on a testé de récupérer l'image ;)");
             img = JSONHelper.FromBase64(substrings[1]);
         }
         catch (Exception e)
         {
-            Debug.Log("erreur lors de la récupération de l'image");
+            Debug.Log("Erreur lors de la récupération de l'image");
             img = JSONHelper.FromBase64(picture);
         }
         Texture2D tex = new Texture2D(2, 2);
         tex.LoadImage(img);
-        GetComponent<Image>().material.mainTexture = tex;
+        researchImage.material = new Material(researchImage.material);
+        researchImage.material.mainTexture = tex;
     }
 }
 
