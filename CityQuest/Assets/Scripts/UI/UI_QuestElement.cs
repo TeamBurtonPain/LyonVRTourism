24812818a8c2b8c48936358c4a41476e34af651a
HubertHamelin <32563790+HubertHamelin@users.noreply.github.com>
2018-05-03 17:48:22 +0200

    Correction PROGRESS
    la barre de progrès ne doit dépendre plus que du nombre de checkpoints déjà faits

--- CityQuest/Assets/Scripts/UI/UI_QuestElement.cs
+++ CityQuest/Assets/Scripts/UI/UI_QuestElement.cs
@@ -18,8 +18,22 @@ public class UI_QuestElement : MonoBehaviour
     {
         myQuest = sQuest.Quest;
         questNameText.text = sQuest.Quest.Title;
-        questProgressText.text = sQuest.Score.ToString();
-        circularBar.fillAmount = (float) sQuest.Score;
+        //questProgressText.text = sQuest.Score.ToString();
+        //circularBar.fillAmount = (float) sQuest.Score;
+        int checkpointsFinished = 0;
+        int checkpointsTotal = 0;
+        foreach (StateCheckPoint stateCheckpoint in sQuest.Checkpoints)
+        {
+            if(stateCheckpoint.Status == StatusCheckPoint.FINISHED)
+            {
+                checkpointsFinished++;
+            }
+            checkpointsTotal++;
+        }
+        float progress = checkpointsFinished / checkpointsTotal;
+        circularBar.fillAmount = progress;
+        questProgressText.text = "" + Mathf.Ceil(progress * 100) + "%";
+
         badges = Controller.Instance.User.Badges;
         List<CheckPoint> checkpoints = myQuest.Checkpoints;
         FillBadgesList(checkpoints);
