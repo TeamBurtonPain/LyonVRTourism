73c4030b2ed4f4b16aac9344ef63ba6de3c34e8c
cpourcel <clara.pourcel@insa-lyon.fr>
2018-05-03 14:15:23 +0200

    [CP] Affichage front nouveaux badges

---
+++ CityQuest/Assets/Scripts/UI/Play/InformationsManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+class InformationsManager : MonoBehaviour
+{
+    public Text InformationText;
+    public Transform Parent;
+    public Canvas NewBadgePanel;
+
+    void Start()
+    {
+        InformationText.text = PlayQuestController.Instance.CurrentCheckpoint.Checkpoint.Text;
+    }
+    public void Btn_Next ()
+    {
+        if(PlayQuestController.Instance.CurrentCheckpoint.Checkpoint.IdBadge != null)
+        {
+           Instantiate(NewBadgePanel, this.Parent);
+        } else
+         {
+            PlayQuestController.Instance.GoNextScene();
+        }
+    }
+}
+
